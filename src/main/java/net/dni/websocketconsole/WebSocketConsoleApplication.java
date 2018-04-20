7a5735c8eae4c3b8eafe50d7695129e51ab6c349
dni-developer <dni.developer@hotmail.com>
2018-04-19 21:36:25 -0400

    create mave project

---
+++ src/main/java/net/dni/websocketconsole/WebSocketConsoleApplication.java
@@ -0,0 +1,12 @@
+package net.dni.websocketconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketConsoleApplication.class, args);
+	}
+}
