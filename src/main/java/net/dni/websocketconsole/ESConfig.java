17c0ce54e58247c8084362c8098ef1f89962aa48
dni-developer <dni.developer@hotmail.com>
2018-04-29 22:41:25 -0400

    Revert "change spring boot version to 1.5.12"
    This reverts commit c30992e

--- src/main/java/net/dni/websocketconsole/ESConfig.java
+++ src/main/java/net/dni/websocketconsole/ESConfig.java
@@ -1,24 +1,10 @@
 package net.dni.websocketconsole;
 
-import org.elasticsearch.node.NodeBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-
-//https://docs.spring.io/spring-data/elasticsearch/docs/current/reference/html/#elasticsearch.annotation
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "net.dni.websocketconsole.repository")
 public class ESConfig {
-    @Bean
-    public NodeBuilder nodeBuilder() {
-        return new NodeBuilder();
-    }
 
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
-    }
 }
